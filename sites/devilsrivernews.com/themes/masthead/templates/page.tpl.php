9de8c87172c5976185f259cde5297dd24514e492
etype <etype@texas>
2016-01-30 01:42:07 +0000

    fixes

--- sites/devilsrivernews.com/themes/masthead/templates/page.tpl.php
+++ sites/devilsrivernews.com/themes/masthead/templates/page.tpl.php
@@ -132,6 +132,7 @@
     <footer id="footer" class="<?php echo $grid_full_width ?>">
       <?php print render($page['footer']) ?>
      <!--The following line cannot be changed or removed without violating the terms and service of Surf New Media. Site will be shut down within 24 hours of removal -->   <span style="font-size:0.8em;"><a href="http://www.surfnewmedia.com">Surf New Media</a></span>
+     <span style="font-size:0.8em;">Copyright <?php echo date("Y"); ?> Masked Rider Publishing, Inc.</span>
     </footer><!-- /footer -->
   </div></div>
  

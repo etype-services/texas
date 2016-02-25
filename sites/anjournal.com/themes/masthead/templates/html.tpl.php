e0f5e8ba3f550a9c5f784d36e755814853f7c819
etype <etype@texas>
2016-02-25 03:18:29 +0000

    updates

---
+++ sites/anjournal.com/themes/masthead/templates/html.tpl.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
+
+<head>
+<style>
+ .sf-menu.sf-style-default a {
+ /* border-left: 1px solid #000 !important;
+  border-top: 1px solid #000 !important;
+  padding: 0.75em 1em !important; */
+  }
+  .sf-menu.sf-style-pomegranate li, .sf-menu.sf-style-pomegranate.sf-navbar {
+  background: #A8141A !important;
+  text-transform: uppercase !important;
+  font-size: 15px !important;
+  border-bottom:0px !important;
+  height:38px !important;
+  min-height:38px !important;
+  }
+  .sf-menu.sf-style-pomegranate a
+  {
+color: #ffebee !important; 
+  text-shadow:0px 0px !important;
+  border:0px !important;
+  padding: 0em 1em !important;
+  height:38px !important;
+  line-height:38px !important;
+  } 
+ .sf-menu.sf-style-pomegranate li:hover, .sf-menu.sf-style-pomegranate li.sfHover, .sf-menu.sf-style-pomegranate li.active a, .sf-menu.sf-style-pomegranate a:focus, .sf-menu.sf-style-pomegranate a:hover, .sf-menu.sf-style-pomegranate a:active, .sf-menu.sf-style-pomegranate.sf-navbar li li
+ {
+   background: #000 !important;
+  color: #ffffff !important;
+}
+ .sf-menu.sf-style-pomegranate a:hover { text-decoration:underline !important; line-height: 38px !important;}
+ .block-inner {overflow:hidden;}
+ #block-simpleads-ad-groups-65 .block-inner .content .adslist {margin-top:15px;}
+ .preface-wrapper .block .content {  height: 310px !important;
+  overflow: hidden !important;}
+  .postscript-wrapper {
+  padding: 20px 0;
+  margin: 0 -10px;
+  background: url(/sites/advance/themes/masthead/images/style1/postscript-bg.png) !important; }
+  .postscript-wrapper h3 span {
+  padding-right: 5px !important;
+  background: #333333 !important;
+  color: #999999 !important;
+  font-size: 11px;
+  font-weight: normal;
+}
+</style>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>  
+ 
+<meta name="description" content="Advance Publishing Company | Advance News Journal">
+<meta name="keywords" content=" Advance Publishing Company | Advance News Journal">
+<meta name="author" content="http://theadvancenewsjournal.com"/>
+<meta name="rating" content="General"/>
+<meta name="revisit-after" content="7 days"/>
+<meta name="robots" content="index, follow"/>
+<meta property="og:image" content="http://anjournal.com/sites/advance/files/400pxAdvanceforWeb.gif">  
+  
+  
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">  
+  <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+
+<body class="<?php print $classes; ?>"<?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+
+</html>
--- sites/sites.php
+++ sites/sites.php
@@ -6,7 +6,6 @@ $sites['www.uvaldeleadernews.com'] = 'uvaldeleadernews.com';
 $sites['prod.uvaldeleadernews.com'] = 'uvaldeleadernews.com';
 $sites['dev.uvaldeleadernews.com'] = 'uvaldeleadernews.com';
 
-
 $sites['devilsrivernews.etypegoogle5.com'] = 'devilsrivernews.com';
 $sites['devilsrivernews.com'] = 'devilsrivernews.com';
 $sites['www.devilsrivernews.com'] = 'devilsrivernews.com';
@@ -18,7 +17,6 @@ $sites['www.devilsriver.news'] = 'devilsrivernews.com';
 $sites['prod.devilsriver.news'] = 'devilsrivernews.com';
 $sites['dev.devilsriver.news'] = 'devilsrivernews.com';
 
-
 $sites['yourforneymessenger.etypegoogle5.com'] = 'yourforneymessenger.com';
 $sites['yourforneymessenger.com'] = 'yourforneymessenger.com';
 $sites['www.yourforneymessenger.com'] = 'yourforneymessenger.com';
@@ -29,3 +27,37 @@ $sites['forneymessengernewspaper.com'] = 'yourforneymessenger.com';
 $sites['www.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
 $sites['prod.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
 $sites['dev.forneymessengernewspaper.com'] = 'yourforneymessenger.com';
+
+$sites['jdcmountaindispatch.etypegoogle5.com'] = 'jdcmountaindispatch.com';
+$sites['jdcmountaindispatch.com'] = 'jdcmountaindispatch.com';
+$sites['www.jdcmountaindispatch.com'] = 'jdcmountaindispatch.com';
+$sites['prod.jdcmountaindispatch.com'] = 'jdcmountaindispatch.com';
+$sites['dev.jdcmountaindispatch.com'] = 'jdcmountaindispatch.com';
+
+$sites['martincountymessenger.etypegoogle5.com'] = 'martincountymessenger.com';
+$sites['martincountymessenger.com'] = 'martincountymessenger.com';
+$sites['www.martincountymessenger.com'] = 'martincountymessenger.com';
+$sites['prod.martincountymessenger.com'] = 'martincountymessenger.com';
+$sites['dev.martincountymessenger.com'] = 'martincountymessenger.com';
+
+$sites['anjournal.etypegoogle5.com'] = 'anjournal.com';
+$sites['anjournal.com'] = 'anjournal.com';
+$sites['www.anjournal.com'] = 'anjournal.com';
+$sites['prod.anjournal.com'] = 'anjournal.com';
+$sites['dev.anjournal.com'] = 'anjournal.com';
+
+$sites['advancepublishingcompany.com'] = 'anjournal.com';
+$sites['www.advancepublishingcompany.com'] = 'anjournal.com';
+$sites['prod.advancepublishingcompany.com'] = 'anjournal.com';
+$sites['dev.advancepublishingcompany.com'] = 'anjournal.com';
+
+$sites['theadvancenewsjournal.com'] = 'anjournal.com';
+$sites['www.theadvancenewsjournal.com'] = 'anjournal.com';
+$sites['prod.theadvancenewsjournal.com'] = 'anjournal.com';
+$sites['dev.theadvancenewsjournal.com'] = 'anjournal.com';
+
+$sites['silsbeebee.etypegoogle5.com'] = 'silsbeebee.com';
+$sites['silsbeebee.com'] = 'silsbeebee.com';
+$sites['www.silsbeebee.com'] = 'silsbeebee.com';
+$sites['prod.silsbeebee.com'] = 'silsbeebee.com';
+$sites['dev.silsbeebee.com'] = 'silsbeebee.com';

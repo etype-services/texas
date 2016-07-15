2189b8dcbc719a27e06cd041f18863d4d434d9d2
Charlie Llewellin <charliellewellin@gmail.com>
2016-07-15 09:56:49 -0500

    fayette county record

---
+++ sites/fayettecountyrecord.com/themes/md_thenews/templates/coupons.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charlie
+ * Date: 7/15/16
+ * Time: 9:56 a.m.
+ */
+?>
+
+<noscript>
+  <p>Coupons powered by <a href="http://www.coupons.com?pid=13903&nid=10&zid=xh20&bid=1417300001">Coupons.com</a></p>
+</noscript><script id="scriptId_718x940_148741" type="text/javascript" src="//bcg.coupons.com/?scriptId=148741&bid=1417300001&format=718x940&bannerType=3">
+</script>

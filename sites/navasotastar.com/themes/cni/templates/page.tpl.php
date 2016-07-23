2563a50fb0b4f90dfef1847003664261f8964c63
Charlie Llewellin <charlie@llewellin.net>
2016-07-23 10:41:56 -0500

    add navasota star update type module

--- sites/navasotastar.com/themes/cni/templates/page.tpl.php
+++ sites/navasotastar.com/themes/cni/templates/page.tpl.php
@@ -99,9 +99,9 @@
 
     <div class="postscript-wrapper"><div class="postscript-wrapper-inner <?php echo $grid_size ?>">
         <section id="postscript" class="clearfix">
-          <div class="grid_4"><?php print render($page['postscript_1']); ?></div>
-          <div class="grid_4"><?php print render($page['postscript_2']); ?></div>
-          <div class="grid_4"><?php print render($page['postscript_3']); ?></div>
+          <div class="<?php echo $postscript_1_grid_width ?>"><?php print render($page['postscript_1']); ?></div>
+          <div class="<?php echo $postscript_2_grid_width ?>"><?php print render($page['postscript_2']); ?></div>
+          <div class="<?php echo $postscript_3_grid_width ?>"><?php print render($page['postscript_3']); ?></div>
         </section>
       </div></div>
   </div>

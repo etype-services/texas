c6a1df7a079a982dfabf9f482ba5e20616e81e40
root <root@delphos.c.modern-photon-112115.internal>
2016-09-06 06:33:33 -0500

    changes from subljme

--- lifestyle.php
+++ lifestyle.php
@@ -1,3 +1,11 @@
 <?php
-require_once('sites/eastlandcountytoday.com/rssfeed/lifestyle.inc');
+$url = $_SERVER['HTTP_HOST'];
+if($url=='ourtowntimes.com' || $url=='www.ourtowntimes.com')
+{
+ require_once('sites/ourtowntimes.com/rssfeed/lifestyle.inc');
+}
+else if($url == 'eastlandcountytoday.com' || $url == 'www.eastlandcountytoday.com')
+{
+ require_once('sites/eastlandcountytoday.com/rssfeed/lifestyle.inc');
+}
 ?>

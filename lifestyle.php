7e5d90345bd218ec4c53ae6a4ebfb3f39b8fce91
root <root@delphos.c.modern-photon-112115.internal>
2016-09-06 07:14:04 -0500

    changes from sublime

--- lifestyle.php
+++ lifestyle.php
@@ -1,10 +1,6 @@
 <?php
 $url = $_SERVER['HTTP_HOST'];
-if($url=='ourtowntimes.com' || $url=='www.ourtowntimes.com')
-{
- require_once('sites/ourtowntimes.com/rssfeed/lifestyle.inc');
-}
-else if($url == 'eastlandcountytoday.com' || $url == 'www.eastlandcountytoday.com')
+if($url == 'eastlandcountytoday.com' || $url == 'www.eastlandcountytoday.com')
 {
  require_once('sites/eastlandcountytoday.com/rssfeed/lifestyle.inc');
 }

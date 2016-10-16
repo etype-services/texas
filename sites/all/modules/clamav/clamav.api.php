aec0c94e0235c2d1fe74f56c1add3e142033d652
etype <etype@texas>
2016-10-16 17:55:40 -0500

    updates

--- sites/all/modules/clamav/clamav.api.php
+++ sites/all/modules/clamav/clamav.api.php
@@ -11,10 +11,40 @@
  */
 
 /**
- * Act on an uploaded file being scanned by ClamAV.
+ * Alter whether a file should be scanned by the ClamAV anti-virus scanner.
  *
- * Modules implementing this hook can prevent a file being scanned by ClamAV
- * during the file upload process.
+ * By default, all files are considered scannable.
+ *
+ * Examples of use-cases where a scan may not be not wanted include:
+ *
+ * - Files considered "safe".
+ *   Certain file-types have often been considered "safe", for example, it was
+ *   thought that image files could not contain a virus (although this has
+ *   since been proved incorrect).
+ *
+ * - Extremely large files.
+ *   Invoking the scanner adds overhead, and the larger the file, the longer it
+ *   could take to scan. Scanning a large file could cause the Drupal process
+ *   to time-out.
+ *
+ * - Alternative backend storage.
+ *   If a file is being saved directly into an alternative backend storage (for
+ *   example, a CDN or a cloud file-storage system), it may already be checked
+ *   for viruses in the alternative handler, in which case duplicate checking
+ *   would be unnecessary.
+ *
+ * - Deliberate storage of a virus.
+ *   Some organisations have a business need to store infected files. Security
+ *   researchers, anti-virus organisations, and many others have a genuine
+ *   requirement to allow an infected file to be saved, although they may also
+ *   require other parts of the site to be protected.
+ *
+ * There are many genuine use-cases where virus-scanning should be
+ * conditionally disabled for certain files. Modules can choose to require the
+ * file is scanned, stop the file from being scanned, or not affect the result.
+ *
+ * Modules are processed in weight order. The "heaviest" module has the final
+ * decision on whether the file is scanned.
  *
  * @param $file
  *   A file entity.

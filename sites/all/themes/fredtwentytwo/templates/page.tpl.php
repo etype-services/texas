30584d2f68b0c429bc62ac14e56aec1f4b87e4a5
Charlie Llewellin <charlie@llewellin.net>
2017-01-28 18:20:10 -0600

    fisher county

--- sites/all/themes/fredtwentytwo/templates/page.tpl.php
+++ sites/all/themes/fredtwentytwo/templates/page.tpl.php
@@ -87,7 +87,6 @@
 
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-	  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="mobile-logo"><img src="<?php print $base_path; ?><?php print $directory; ?>/logo_mobile.png" alt="<?php print t('Home'); ?>" /></a>
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>

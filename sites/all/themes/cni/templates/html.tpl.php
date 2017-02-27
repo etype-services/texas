7ed0a46a485649c941a1b1b74921bcce5fb4bb4b
Charlie Llewellin <charlie@llewellin.net>
2017-02-27 11:16:39 -0500

    theme tweaks

--- sites/all/themes/cni/templates/html.tpl.php
+++ sites/all/themes/cni/templates/html.tpl.php
@@ -6,13 +6,28 @@
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
     <?php print $scripts; ?>
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet'
-          type='text/css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+
+    <!-- begin code for etype google dfp ads -->
+    <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+    <script>
+        var googletag = googletag || {};
+        googletag.cmd = googletag.cmd || [];
+    </script>
+
+    <script>
+        googletag.cmd.push(function() {
+            googletag.defineSlot('/116205717/eTypeleaderboard', [728, 90], 'div-gpt-ad-1487352373268-0').addService(googletag.pubads());
+            googletag.defineSlot('/116205717/eTypesidebar', [300, 250], 'div-gpt-ad-1487352373268-1').addService(googletag.pubads());
+            googletag.pubads().enableSingleRequest();
+            googletag.enableServices();
+        });
+    </script>
+    <!-- end code for etype google dfp ads -->
 </head>
 
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
@@ -33,5 +48,4 @@
 <?php print $page; ?>
 <?php print $page_bottom; ?>
 </body>
-
 </html>

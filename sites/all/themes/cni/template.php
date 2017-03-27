8c77c7160e9956d3f44cd701db9db32821431403
Charlie Llewellin <charlie@llewellin.net>
2017-03-27 16:27:28 -0500

    dfp header code

--- sites/all/themes/cni/template.php
+++ sites/all/themes/cni/template.php
@@ -148,9 +148,8 @@ function cni_preprocess_html(&$variables) {
   }
 
   $dfp_code = theme_get_setting('dfp_code');
-  echo $dfp_code;
-  if ($dfp_code == 1) {
-    $variables['dfp_code'] == 1;
+  if ($dfp_code == '1') {
+    $variables['dfp_code'] = 1;
   }
 
   /* add site setting css */

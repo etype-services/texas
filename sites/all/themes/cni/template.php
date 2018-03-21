adcd0b8c69021298a4e788e22628885b7d11c797
etype <etype@texas>
2018-03-21 09:37:33 -0500

    updates

--- sites/all/themes/cni/template.php
+++ sites/all/themes/cni/template.php
@@ -323,3 +323,19 @@ function get_grid_info() {
   return $grid_info;
 
 }
+
+/**
+ * @param $vars
+ */
+function cni_preprocess_views_view_row_rss(&$vars) {
+  $item = $vars['row'];
+  $result = $vars['view']->result;
+  $id = $vars['id'];
+  $node = node_load( $result[$id-1]->nid );
+  $vars['title'] = trim(check_plain($item->title));
+  $vars['link'] = check_url($item->link);
+  $vars['description'] = check_plain($item->description);
+  $vars['node'] = $node;
+  $vars['item_elements'] = empty($item->elements) ? '' : format_xml_elements($item->elements);
+  empty($node->field_image['und'][0]['uri'])? $vars['img'] = '': $vars['img']  = file_create_url($node->field_image['und'][0]['uri']);
+}

519ab0bc538d24c5498e12a7a776ee0beba055a8
etype <etype@texas>
2018-03-21 09:44:25 -0500

    cni theme

--- sites/all/themes/cni/templates/views-view-row-rss--gorman-rss-feed--gorman-feed.tpl.php
+++
@@ -1,7 +0,0 @@
-<item>
-    <title><?php print $title; ?></title>
-    <link><?php print $link; ?></link>
-    <image><?php print $img;?></image>
-    <description><?php print $description;?></description>
-    <?php print $item_elements; ?>
-</item>

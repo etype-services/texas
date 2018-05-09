7b570509b0a63710b4ddbe575f8b997446deff94
Charlie Llewellin <charlie@llewellin.net>
2018-05-09 18:45:57 -0500

    gatesville advanced poll

--- sites/sites.php
+++ sites/sites.php
@@ -231,10 +231,10 @@ $sites['wingspannewspaper.etypegoogle5.com'] = 'wingspannewspaper.etypegoogle5.c
 $sites['wingspancorpuschristi.com'] = 'wingspannewspaper.etypegoogle5.com';
 $sites['www.wingspancorpuschristi.com'] = 'wingspannewspaper.etypegoogle5.com';
 
-/* marfabigbendsentinel.etypegoogle5.com (bigbendnow.com) */
-$sites['bigbendnow.etypegoogle5.com'] = 'marfabigbendsentinel.etypegoogle5.com';
-$sites['bigbendnow.com'] = 'marfabigbendsentinel.etypegoogle5.com';
-$sites['www.bigbendnow.com'] = 'marfabigbendsentinel.etypegoogle5.com';
+/* bigbendnow.etypegoogle5.com */
+$sites['bigbendnow.etypegoogle5.com'] = 'bigbendnow.etypegoogle5.com';
+$sites['bigbendnow.com'] = 'bigbendnow.etypegoogle5.com';
+$sites['www.bigbendnow.com'] = 'bigbendnow.etypegoogle5.com';
 
 /* testing */
 $sites['dfp.etypegoogle5.com'] = 'gatesvillemessenger.etypegoogle5.com';

a8b14edaa8054e556a27655f581081b894f3b3fe
Charlie Llewellin <charlie@llewellin.net>
2019-04-17 14:28:37 -0500

    add facebook pixel code to fayetteville

--- sites/fayettecountyrecord.com/themes/md_thenews/templates/html.tpl.php
+++ sites/fayettecountyrecord.com/themes/md_thenews/templates/html.tpl.php
@@ -23,20 +23,6 @@
 	<?php if (isset($custom_css)): print $custom_css; endif; ?>
 </style>
 <?php if (isset($header_code)): print $header_code; endif;?>
-    <!-- Facebook Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '308322346502400');
-        fbq('track', 'PageView');
-    </script>
-    <!-- End Facebook Pixel Code —>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">

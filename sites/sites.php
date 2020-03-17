d2ef540ccba621b88bb0ad67a1fa122250e5389d
Charlie Llewellin <charlie@llewellin.net>
2020-03-17 12:20:21 -0500

    site.php

--- sites/sites.php
+++ sites/sites.php
@@ -2,235 +2,46 @@
 /* test.etypegoogle5.com */
 $sites['test.etypegoogle5.com'] = 'test.etypegoogle5.com';
 $sites['cni.local'] = 'test.etypegoogle5.com';
-
-/* cni-theme.etypegoogle5.com */
 $sites['cni-theme.etypegoogle5.com'] = 'cni-theme.etypegoogle5.com';
 
-/* devilsrivernews.etypegoogle5.com */
-$sites['devilsrivernews.etypegoogle5.com'] = 'devilsrivernews.com';
-$sites['devilsrivernews.com'] = 'devilsrivernews.com';
-$sites['www.devilsrivernews.com'] = 'devilsrivernews.com';
-$sites['devilsriver.news'] = 'devilsrivernews.com';
-$sites['www.devilsriver.news'] = 'devilsrivernews.com';
-
-/* yourforneymessenger.etypegoogle5.com */
-$sites['yourforneymessenger.etypegoogle5.com'] = 'yourforneymessenger.etypegoogle5.com';
-$sites['yourforneymessenger.com'] = 'yourforneymessenger.etypegoogle5.com';
 $sites['www.yourforneymessenger.com'] = 'yourforneymessenger.etypegoogle5.com';
-$sites['forneymessengernewspaper.com'] = 'yourforneymessenger.etypegoogle5.com';
 $sites['www.forneymessengernewspaper.com'] = 'yourforneymessenger.etypegoogle5.com';
-$sites['forneymessenger.com'] = 'yourforneymessenger.etypegoogle5.com';
 $sites['www.forneymessenger.com'] = 'yourforneymessenger.etypegoogle5.com';
 $sites ['archive.yourforneymessenger.com'] = 'yourforneymessengerarchive.etypegoogle5.com';
-
-/* jdcmountaindispatch.etypegoogle5.com */
-$sites['jdcmountaindispatch.etypegoogle5.com'] = 'jdcmountaindispatch.com';
-$sites['jdcmountaindispatch.com'] = 'jdcmountaindispatch.com';
-$sites['www.jdcmountaindispatch.com'] = 'jdcmountaindispatch.com';
-
-/* martincountymessenger.etypegoogle5.com */
-$sites['martincountymessenger.etypegoogle5.com'] = 'martincountymessenger.com';
-$sites['martincountymessenger.com'] = 'martincountymessenger.com';
+$sites['www.jdcmountaindispatch.com'] = 'jdcmountaindispatch.com';;
 $sites['www.martincountymessenger.com'] = 'martincountymessenger.com';
-
-/* anjournal.etypegoogle5.com */
-$sites['anjournal.etypegoogle5.com'] = 'archive.anjournal.com';
 $sites['www.anjournal.com'] = 'archive.anjournal.com';
 $sites['archive.anjournal.com'] = 'archive.anjournal.com';
-$sites['advancepublishingcompany.com'] = 'archive.anjournal.com';
-$sites['www.advancepublishingcompany.com'] = 'archive.anjournal.com';
-$sites['theadvancenewsjournal.com'] = 'archive.anjournal.com';
-$sites['www.theadvancenewsjournal.com'] = 'archive.anjournal.com';
-
-/* silsbeebee.etypegoogle5.com */
 $sites['www.silsbeebee.com'] = 'silsbeebee.etypegoogle5.com';
 $sites['archive.silsbeebee.com'] = 'archive.silsbeebee.etypegoogle5.com';
-
-$sites['coveleaderpress.etypegoogle5.com'] = 'coveleaderpress.com';
-$sites['coveleaderpress.com'] = 'coveleaderpress.com';
 $sites['www.coveleaderpress.com'] = 'coveleaderpress.com';
-
-$sites['perrytonherald.etypegoogle5.com'] = 'perrytonherald.com';
-$sites['perrytonherald.com'] = 'perrytonherald.com';
 $sites['www.perrytonherald.com'] = 'perrytonherald.com';
-
-$sites['mybiglake.etypegoogle5.com'] = 'mybiglake.com';
-$sites['mybiglake.com'] = 'mybiglake.com';
-$sites['www.mybiglake.com'] = 'mybiglake.com';
-
-$sites['cantonguide.etypegoogle5.com'] = 'cantonguide.com';
-$sites['cantonguide.com'] = 'cantonguide.com';
-$sites['www.cantonguide.com'] = 'cantonguide.com';
-
-$sites['gonzalescannon.etypegoogle5.com'] = 'gonzalescannon.com';
-$sites['gonzalescannon.com'] = 'gonzalescannon.com';
-$sites['www.gonzalescannon.com'] = 'gonzalescannon.com';
-
-/* theedenecho.etypegoogle5.com */
-$sites['theedenecho.etypegoogle5.com'] = 'theedenecho.com';
-$sites['theedenecho.com'] = 'theedenecho.com';
-$sites['www.theedenecho.com'] = 'theedenecho.com';
-$sites['edenecho.net'] = 'theedenecho.com';
-$sites['www.edenecho.net'] = 'theedenecho.com';
-
-/* eastlandcountytoday.etypegoogle5.com */
-$sites['eastlandcountytoday.etypegoogle5.com'] = 'eastlandcountytoday.etypegoogle5.com';
-$sites['eastlandcountytoday.com'] = 'eastlandcountytoday.etypegoogle5.com';
 $sites['www.eastlandcountytoday.com'] = 'eastlandcountytoday.etypegoogle5.com';
 $sites['archive.eastlandcountytoday.com'] = 'eastlandcountytodayarchive.etypegoogle5.com';
-
-$sites['eldoradosuccess.etypegoogle5.com'] = 'eldoradosuccess.com';
-$sites['eldoradosuccess.com'] = 'eldoradosuccess.com';
-$sites['www.eldoradosuccess.com'] = 'eldoradosuccess.com';
-
-$sites['myeldorado.net'] = 'eldoradosuccess.com';
-$sites['www.myeldorado.net'] = 'eldoradosuccess.com';
-
-/* fayettecountyrecord.etypegoogle5.com */
-$sites['fayettecountyrecord.etypegoogle5.com'] = 'fayettecountyrecord.com';
-$sites['fayettecountyrecord.com'] = 'fayettecountyrecord.com';
 $sites['www.fayettecountyrecord.com'] = 'fayettecountyrecord.com';
-$sites['fayettecountyrecorddev.etypegoogle5.com'] = 'fayettecountyrecord.com';
-
-/* fishercountychronicle.etypegoogle5.com */
-$sites['fishercountychronicle.etypegoogle5.com'] = 'fishercountychronicle.com';
-$sites['fishercountychronicle.com'] = 'fishercountychronicle.com';
 $sites['www.fishercountychronicle.com'] = 'fishercountychronicle.com';
 $sites['www.doublemountainchronicle.com'] = 'fishercountychronicle.com';
-
-$sites['normangeestar.etypegoogle5.com'] = 'normangeestar.com';
-$sites['normangeestar.com'] = 'normangeestar.com';
-$sites['www.normangeestar.com'] = 'normangeestar.com';
-
-/* easttexaspress.etypegoogle5.com */
-$sites['easttexaspress.etypegoogle5.com'] = 'easttexaspress.com';
-$sites['easttexaspress.com'] = 'easttexaspress.com';
 $sites['www.easttexaspress.com'] = 'easttexaspress.com';
-
-$sites['ourtowntimes.com'] = 'easttexaspress.com';
 $sites['www.ourtowntimes.com'] = 'easttexaspress.com';
-
-$sites['theeasttexaspress.com'] = 'easttexaspress.com';
-$sites['www.theeasttexaspress.com'] = 'easttexaspress.com';
-
-$sites['inbetweenprint.com'] = 'easttexaspress.com';
 $sites['www.inbetweenprint.com'] = 'easttexaspress.com';
-
-$sites['inbetweenprint.net'] = 'easttexaspress.com';
-$sites['www.inbetweenprint.net'] = 'easttexaspress.com';
-
-$sites['inbetweenprint.info'] = 'easttexaspress.com';
-$sites['www.inbetweenprint.info'] = 'easttexaspress.com';
-
-$sites['inbetweenprint.org'] = 'easttexaspress.com';
-$sites['www.inbetweenprint.org'] = 'easttexaspress.com';
-
-$sites['ottnews.co'] = 'easttexaspress.com';
-$sites['www.ottnews.co'] = 'easttexaspress.com';
-
-$sites['ott.news'] = 'easttexaspress.com';
-$sites['www.ott.news'] = 'easttexaspress.com';
-
-/* snyderdailynews.etypegoogle5.com */
-$sites['snyderdailynews.etypegoogle5.com'] = 'snyderdailynews.com';
-$sites['snyderdailynews.com'] = 'snyderdailynews.com';
 $sites['www.snyderdailynews.com'] = 'snyderdailynews.com';
 $sites['www.thesnydernews.com'] = 'snyderdailynews.com';
-
-/* thevidorian.etypegoogle5.com */
-$sites['thevidorian.etypegoogle5.com'] = 'thevidorian.com';
-$sites['thevidorian.com'] = 'thevidorian.com';
 $sites['www.thevidorian.com'] = 'thevidorian.com';
-
-/* seniorific.etypegoogle5.com */
-$sites['seniorific.etypegoogle5.com'] = 'seniorific.com';
-$sites['seniorific.com'] = 'seniorific.com';
 $sites['www.seniorific.com'] = 'seniorific.com';
 $sites['seniorific-archives.etypegoogle5.com'] = 'seniorific-archives.etypegoogle5.com';
-
-/* ozonastockman.etypegoogle5.com */
-$sites['ozonastockman.etypegoogle5.com'] = 'ozonastockman.com';
-$sites['ozonastockman.com'] =  'ozonastockman.com';
 $sites['www.ozonastockman.com'] = 'ozonastockman.com';
-
-/* quinlan-tawakoninews.etypegoogle5.com */
-$sites['quinlan-tawakoninews.etypegoogle5.com'] =  'archive.quinlan-tawakoninews.com';
-$sites['archive.quinlan-tawakoninews.com'] =  'archive.quinlan-tawakoninews.com';
-
-/* thecantonherald.etypegoogle5.com */
-$sites['thecantonherald.etypegoogle5.com'] =  'archive.thecantonherald.com';
 $sites['archive.thecantonherald.com'] =  'archive.thecantonherald.com';
-
-/* vanbanner.etypegoogle5.com */
-$sites['vanbanner.etypegoogle5.com'] =  'vanbanner.etypegoogle5.com';
-$sites['vanbanner.com'] =  'vanbanner.etypegoogle5.com';
-$sites['www.vanbanner.com'] =  'vanbanner.etypegoogle5.com';
-
-/* willspointchronicle.etypegoogle5.com */
-$sites['willspointchronicle.etypegoogle5.com'] =  'archive.willspointchronicle.etypegoogle5.com';
 $sites['archive.willspointchronicle.com'] =  'archive.willspointchronicle.etypegoogle5.com';
-
-/* sabinecountyreporter.etypegoogle5.com */
-$sites['sabinecountyreporter.etypegoogle5.com'] =  'sabinecountyreporter.etypegoogle5.com';
-$sites['sabinecountyreporter.com'] =  'sabinecountyreporter.etypegoogle5.com';
 $sites['www.sabinecountyreporter.com'] =  'sabinecountyreporter.etypegoogle5.com';
-
-/* gatesvillemessenger.etypegoogle5.com */
-$sites['gatesvillemessenger.etypegoogle5.com'] = 'gatesvillemessenger.etypegoogle5.com';
-$sites['gatesvillemessenger.com'] = 'gatesvillemessenger.etypegoogle5.com';
 $sites['www.gatesvillemessenger.com'] = 'gatesvillemessenger.etypegoogle5.com';
-
-/* rockspringsrecord.etypegoogle5.com */
-$sites['rockspringsrecord.etypegoogle5.com'] = 'rockspringsrecord.etypegoogle5.com';
-$sites['rockspringsrecord.com'] = 'rockspringsrecord.etypegoogle5.com';
 $sites['www.rockspringsrecord.com'] = 'rockspringsrecord.etypegoogle5.com';
-
-/* timestribune.etypegoogle5.com */
-$sites['timestribune.etypegoogle5.com'] = 'timestribune.etypegoogle5.com';
-$sites['timestribune.com'] = 'timestribune.etypegoogle5.com';
 $sites['www.timestribune.com'] = 'timestribune.etypegoogle5.com';
-
-/* baycitysentinel.etypegoogle5.com */
-$sites['baycitysentinel.etypegoogle5.com'] = 'baycitysentinel.etypegoogle5.com';
-$sites['baycitysentinel.com'] = 'baycitysentinel.etypegoogle5.com';
 $sites['www.baycitysentinel.com'] = 'baycitysentinel.etypegoogle5.com';
-
-/* rockdalereporter.etypegoogle5.com */
-$sites['rockdalereporter.etypegoogle5.com'] = 'rockdalereporter.etypegoogle5.com';
-$sites['rockdalereporter.com'] = 'rockdalereporter.etypegoogle5.com';
 $sites['www.rockdalereporter.com'] = 'rockdalereporter.etypegoogle5.com';
-
-/* polkabeat.etypegoogle5.com */
-$sites['polkabeat.etypegoogle5.com'] = 'polkabeat.etypegoogle5.com';
-$sites['texaspolkanews.com'] = 'polkabeat.etypegoogle5.com';
 $sites['www.texaspolkanews.com'] = 'polkabeat.etypegoogle5.com';
-
-/* wingspannewspaper.etypegoogle5.com */
-$sites['wingspannewspaper.etypegoogle5.com'] = 'wingspannewspaper.etypegoogle5.com';
-$sites['wingspancorpuschristi.com'] = 'wingspannewspaper.etypegoogle5.com';
 $sites['www.wingspancorpuschristi.com'] = 'wingspannewspaper.etypegoogle5.com';
-
-/* bigbendnow.etypegoogle5.com */
-$sites['bigbendnow.etypegoogle5.com'] = 'bigbendnow.etypegoogle5.com';
-$sites['bigbendnow.com'] = 'bigbendnow.etypegoogle5.com';
-$sites['www.bigbendnow.com'] = 'bigbendnow.etypegoogle5.com';
-
-/* presidiointernational.etypegoogle5.com */
-$sites['presidiointernational.etypegoogle5.com'] = 'presidiointernational.etypegoogle5.com';
-$sites['presidiointernational.net'] = 'presidiointernational.etypegoogle5.com';
-$sites['www.presidiointernational.net'] = 'presidiointernational.etypegoogle5.com';
-
-/* thealbanynews.etypegoogle5.com */
 $sites['www.thealbanynews.net'] = 'thealbanynews.etypegoogle5.com';
-
-/* thecastrocountynews.etypegoogle5.com */
 $sites['www.thecastrocountynews.com'] = 'thecastrocountynews.etypegoogle5.com';
-
-/* hcnews.etypegoogle5.com */
 $sites['www.hcnews.com'] = 'hcnews.etypegoogle5.com';
-
-/* pressreporter.etypegoogle5.com  */
 $sites['www.pressreporter.com'] = 'pressreporter.etypegoogle5.com';
-
-/* elliscountypress.etypegoogle5.com */
 $sites['www.elliscountypress.com'] = 'elliscountypress.etypegoogle5.com';
